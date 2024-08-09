356bdc85fda2b2bc9eec07952e5ef2430e7e037e
Damiano Bacci <damiano.bacci@gmail.com>
2024-08-09 15:11:59 +0200

    Test map on the canvas using scaling

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,20 @@ console.log("This is the entry file for the project")
 const canvas = document.querySelector('canvas') as HTMLCanvasElement
 
 // Canvas context, will allow us to operate with the Canvas API, it's shorted as "c" because we will call it many time
-const c = canvas.getContext("2d")
-
-console.log(c)
+const c = canvas.getContext("2d") as CanvasRenderingContext2D
 
 canvas.width = 1024
 canvas.height = 576
 console.log(canvas)
+
+const image : HTMLImageElement = new Image()
+image.src = "./images/maps/test.png"
+console.log(image)
+
+image.onload = () => {
+  const scaleFactor = 4
+  c.scale(scaleFactor, scaleFactor)
+  c.imageSmoothingEnabled = false
+  c.drawImage(image, -200, -120)
+  c.setTransform(1, 0, 0, 1, 0, 0)
+}
